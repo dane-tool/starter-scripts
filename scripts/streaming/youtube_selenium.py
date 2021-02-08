6ea6e275aaf7473f39dd44d846b65409ef4b5f4c
saltekar <sahil.altekar1@gmail.com>
2021-02-07 16:02:45 -0800

    streaming script no name error

--- scripts/streaming/youtube_selenium.py
+++ scripts/streaming/youtube_selenium.py
@@ -11,7 +11,7 @@ import time
 # Setup headless browser using Firefox
 driver_options = Options()
 driver_options.headless = True
-driver = selenium.webdriver.Firefox(options=driver_options)
+driver = webdriver.Firefox(options=driver_options)
  
 # Head to the site
 baseurl = 'https://youtube.com'
